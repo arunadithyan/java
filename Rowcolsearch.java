34a54cfc641a60a915b72554b46a3fdf647cd331
Arunaadhi <s.arunadithyan@gmail.comgit config --global user.email s.arunadithyan@gmail.com>
2023-06-26 19:59:28 +0530

    infinite array

---
+++ Rowcolsearch.java
@@ -0,0 +1,33 @@
+package arun;
+
+import java.util.Arrays;
+
+public class Rowcolsearch {
+    public static void main(String[] args) {
+        int[][] arr={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50},};
+
+        int[] result = search(arr,37);
+        System.out.println (Arrays.toString (result));
+
+    }
+     public static int[] search (int[][] matrix,int target){
+        int row=0;
+        int col= matrix.length-1;
+        while(row< matrix.length&&col>=0){
+            if(matrix[row][col]==target){
+                return new int[]{row,col};
+            }
+            if(matrix[row][col]>target){
+                col--;
+            }else{
+                row++;
+            }
+
+        }return new int[]{-1,-1};
+
+    }
+}
