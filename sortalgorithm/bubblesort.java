986456514d637f39b0b423a7a2bfddef621c3cfa
Arunaadhi <s.arunadithyan@gmail.comgit config --global user.email s.arunadithyan@gmail.com>
2023-06-26 22:17:34 +0530

    added sorting algorithms

---
+++ sortalgorithm/bubblesort.java
@@ -0,0 +1,25 @@
+package arun.sortalgorithm;
+
+import java.util.Arrays;
+
+public class bubblesort {
+    public static void main(String[] args) {
+        int[] arr={1,8,3,4,5};
+        bubblesort1(arr);
+
+        System.out.println (Arrays.toString (arr));
+
+    }
+    static void bubblesort1(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+
+            }
+        }
+    }
+}
---
+++ sortalgorithm/selectionsort.java
@@ -0,0 +1,35 @@
+package arun.sortalgorithm;
+
+import java.util.Arrays;
+
+public class selectionsort {
+    public static void main(String[] args) {
+        int[] arr={2,3,56,8,4,3,2};
+        selectionsort1 (arr);
+        System.out.println (Arrays.toString (arr));
+
+    }
+    static void selectionsort1(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-1-i;
+            int maxindex =getmaxindex(arr,0,last);
+//
+            swap (arr,maxindex,last);
+        }
+    }
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+    static int getmaxindex(int[] arr , int start,int end){
+        int max=start;
+        for(int i=start;i<=end;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+
+}
