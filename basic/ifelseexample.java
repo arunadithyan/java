8bf89d8d376eb87b8db5ca0456cb0f5d141b3717
arunaadhi <s.arunadithyan@gmail.com>
2023-07-04 23:05:58 +0530

    added if else

---
+++ basic/ifelseexample.java
@@ -0,0 +1,46 @@
+package arun.basic;
+
+import java.util.Scanner;
+
+public class ifelseexample {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int a =in.nextInt();
+        if(a%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("odd");
+        }
+        System.out.print("Enter the age ");
+        int age=in.nextInt();
+        if(age>=14){
+            System.out.println("old");
+        }else{
+            System.out.println("young");
+        }
+
+        //radix is system number
+        //binary -2 ,octal -8 , hexadecimal -16, decimal -10
+        String num;
+        System.out.print("Enter a number");
+        num=in.next();
+        if(num.matches("[01]+")){
+            System.out.println("Binary number radix =2");
+        }
+        else if (num.matches("[0-7]+")){
+            System.out.println("Octal number radix =8");
+        }
+        else if (num.matches("[0-9]+")){
+            System.out.println("Decimal number radix =10");
+        }
+        else if (num.matches("[0-9A-F]+")){
+            System.out.println("Hexadecimal number radix =16");
+        }
+        else{
+            System.out.println("Invalid number");
+        }
+
+    }
+}
